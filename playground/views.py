f21c48604108b0a1d1e19c2a0ddf1818c9ab014c
Fernando <fernandostarlin1012@gmail.com>
2023-10-16 16:25:11 -0400

    Working with some filtering exercises. The first one: Customers with .com accounts. We also set some settings to our IDE

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Customer
 
 # Create your views here.
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Fernando'})
+    # Filtering Exercises
+
+    # Customers with .com accounts
+    customer_with_dotcom_accounts = Customer.objects.filter(email__endswith='.com')
+    return render(request, 'hello.html', {'customers': customer_with_dotcom_accounts})
