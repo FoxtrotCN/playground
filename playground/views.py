14e76c19bff19d5c7a97c55fac0e5758da85f824
Fernando <fernandostarlin1012@gmail.com>
2023-10-16 16:35:06 -0400

    Exercise 4: Orders placed by customer with id = 1

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, Customer, Category
+from store.models import Product, Customer, Category, Order
 
 # Create your views here.
 
@@ -16,4 +16,7 @@ def say_hello(request):
 
     # Products with low stock
     products_with_low_stock = Product.objects.filter(stock__lt=10)
-    return render(request, 'hello.html', {'products': products_with_low_stock})
+
+    # Orders placed by customer with id=1
+    orders_placed_by_customer1 = Order.objects.filter(customer__id=1)
+    return render(request, 'hello.html', {'orders': orders_placed_by_customer1})
