14e307f6b295760ea15c2c07f548ef14eacc059f
Fernando <fernandostarlin1012@gmail.com>
2023-10-16 18:49:45 -0400

    Learning how to make complex lookups using the Q object in Django

--- playground/views.py
+++ playground/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from store.models import Product, Customer, Category, Order, OrderItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    # Filtering Exercises
-
-    # Customers with .com accounts
-    customer_with_dotcom_accounts = Customer.objects.filter(email__endswith='.com')
-
-    # Categories that don't have a featured product
-    categories_without_featured_product = Category.objects.filter(featured_product__isnull=True)
-
-    # Products with low stock
-    products_with_low_stock = Product.objects.filter(stock__lt=10)
-
-    # Orders placed by customer with id=1
-    orders_placed_by_customer1 = Order.objects.filter(customer__id=1)
-
-    # Order items for products in category 3
-    order_items_for_products_in_category3 = OrderItem.objects.filter(product__category__id=3)
-    return render(request, 'hello.html', {'orderitems': order_items_for_products_in_category3})
+    # Products: Stock < 10 or Price < 20 | Using the Q object that stands for Query
+    queryset = Product.objects.filter(Q(stock__lt=10) | Q(price__lt=20))
+    return render(request, 'hello.html', {'products': queryset})
