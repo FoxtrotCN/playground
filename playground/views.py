4feaab804ca03354cd598f79cde3a45ef9ec97fa
Fernando <fernandostarlin1012@gmail.com>
2023-10-17 18:47:17 -0400

    Learning about Working with Expression Wrappers

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
-from django.db.models import Value, Func, F
+from django.db.models import Value, Func, F, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from store.models import Product, Customer, Category, Order, OrderItem
 
@@ -10,7 +10,8 @@ from store.models import Product, Customer, Category, Order, OrderItem
 
 
 def say_hello(request):
-    queryset = Customer.objects.annotate(
-        orders_count=Count('order')
+    discounted_price = ExpressionWrapper(F('price') * 0.8, output_field=DecimalField())
+    queryset = Product.objects.annotate(
+        discounted_price=discounted_price
     )
     return render(request, 'hello.html', {'queryset': list(queryset)})
