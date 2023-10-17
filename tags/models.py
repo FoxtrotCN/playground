03d6b37e0b56fd0f93e9da471d923e01f1818cd2
Fernando <fernandostarlin1012@gmail.com>
2023-10-17 19:33:00 -0400

    Learning about Custom Managers

--- tags/models.py
+++ tags/models.py
@@ -2,6 +2,13 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 
+
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+        return TaggedItem.objects.select_related('tag').filter(content_type=content_type, object_id=obj_id)
+
+
 # Create your models here.
 
 
@@ -10,6 +17,7 @@ class Tag(models.Model):
 
 
 class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     # What tag applied to what object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     # Type (product, video, article)
