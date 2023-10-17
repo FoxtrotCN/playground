6dd6b65fdfa63c8422430a73ff660dec3cd8d1d7
Fernando <fernandostarlin1012@gmail.com>
2023-10-17 00:13:40 -0400

    Exercise 4: What is the min, max and avg price of products in category 1?

--- playground/views.py
+++ playground/views.py
@@ -21,4 +21,7 @@ def say_hello(request):
     # How many orders has customer 1 placed?
     result = Order.objects.filter(customer__id=1).aggregate(orders_placed=Count('id'))
 
+    # What is the min, max and avg price of products in category 1?
+    result = Product.objects.filter(category__id=3).aggregate(min_price=Min('price'), avg_price=Avg('price'), max_price=Max('price'))
+
     return render(request, 'hello.html', {'result': result})
