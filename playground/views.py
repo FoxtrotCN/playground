30f3840882c534ac95c0921065a20da846718bf6
Fernando <fernandostarlin1012@gmail.com>
2023-10-17 18:24:25 -0400

    Learning about Calling DB functions with the Concat Object from Django. The same result with less code.

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from django.db.models import Value, Func, F
+from django.db.models.functions import Concat
 from store.models import Product, Customer, Category, Order, OrderItem
 
 
@@ -12,5 +13,9 @@ def say_hello(request):
     queryset = Customer.objects.annotate(
         # CONCAT
         full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
+    )
+    queryset = Customer.objects.annotate(
+        # CONCAT
+        full_name=Concat('first_name', Value(' '), 'last_name')
     )
     return render(request, 'hello.html', {'queryset': list(queryset)})
