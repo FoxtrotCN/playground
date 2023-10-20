31dae77cbf05a888bfe2f0b5f8bc2ca9fcf1eb41
Fernando <fernandostarlin1012@gmail.com>
2023-10-19 23:39:40 -0400

    Adding a computed column to the admin site

--- store/admin.py
+++ store/admin.py
@@ -6,9 +6,15 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price']
+    list_display = ['title', 'price', 'stock_status']
     list_editable = ['price']
     list_per_page = 10
+
+    @admin.display(ordering='stock')
+    def stock_status(self, product):
+        if product.stock < 10:
+            return "Low"
+        return "Ok"
 
 
 @admin.register(models.Customer)
